25f374aa41fb89812fe6282da84e95d6a95667d7
Paolo Mosconi <pmosconi@gmail.com>
2021-03-21 19:45:48 +0100

    auth setup and error handling

---
+++ auth/src/errors/database-connection-error.ts
@@ -0,0 +1,19 @@
+import { CustomError } from './custom-error';
+
+export class DatabaseConnectionError extends CustomError {
+    statusCode = 500;
+    reason = 'Error connecting to the database';
+
+    constructor() {
+        super('Error connecting to the database');
+
+        // built in class estension
+        Object.setPrototypeOf(this, DatabaseConnectionError.prototype);
+    }
+
+    serializeErrors() {
+        return [
+            { message: this.reason }
+        ];
+    }
+}
