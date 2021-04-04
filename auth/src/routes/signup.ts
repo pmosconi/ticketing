4e9722b8cc11c0dc6c8ec334a71c51efd0738839
Paolo Mosconi <pmosconi@gmail.com>
2021-04-04 20:15:07 +0200

    signup + signin

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,6 +1,8 @@
 import express, { Request, Response } from 'express';
-import { body, validationResult } from 'express-validator';
-import { RequestValidationError } from '../errors/request-validation-error';
+import { body } from 'express-validator';
+import jwt from 'jsonwebtoken';
+
+import { validateRequest } from '../middlewares/validate-request';
 import { BadRequestError } from '../errors/bad-request-error';
 import { User } from '../models/users';
 
@@ -14,21 +16,30 @@ router.post('/api/users/signup', [
         .trim()
         .isLength({ min: 4, max: 20 })
         .withMessage('Password must be between 4 and 20 characters')
-], async (req: Request, res: Response) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        throw new RequestValidationError(errors.array());
-    }
-    const { email, password } = req.body;
-    const existingUser = await User.findOne({ email });
+    ], 
+    validateRequest,
+    async (req: Request, res: Response) => {
+        const { email, password } = req.body;
+        const existingUser = await User.findOne({ email });
 
-    if (existingUser) {
-        throw new BadRequestError('Email in use');
-    }
+        if (existingUser) {
+            throw new BadRequestError('Email in use');
+        }
 
-    const user = User.build({ email, password });
-    await user.save();
-    res.status(201).send(user);
-});
+        const user = User.build({ email, password });
+        await user.save();
+
+        const userJwt = jwt.sign({
+            id: user.id,
+            email: user.email
+        }, process.env.JWT_KEY!);
+
+        req.session = {
+            jwt: userJwt
+        };
+
+        res.status(201).send(user);
+    }
+);
 
 export { router as signupRouter };
---
+++ start.sh
@@ -0,0 +1,3 @@
+export $(egrep -v '^#' .env.local | xargs)
+kubectl create secret generic jwt-secret --from-literal=JWT_KEY=$JWT_KEY
+skaffold dev
