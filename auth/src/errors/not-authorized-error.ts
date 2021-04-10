40a8bc21d8fad176b30cd10dd53f6283375634c6
Paolo Mosconi <pmosconi@gmail.com>
2021-04-10 16:43:19 +0200

    auth complete

---
+++ auth/src/errors/not-authorized-error.ts
@@ -0,0 +1,18 @@
+import { CustomError } from './custom-error';
+
+export class NotAuthorizedError extends CustomError {
+    statusCode = 401;
+
+    constructor() {
+        super('Not Authorized');
+
+        // built in class estension
+        Object.setPrototypeOf(this, NotAuthorizedError.prototype);
+    }
+
+    serializeErrors() {
+        return [
+            { message: 'Not Authorized' }
+        ];
+    }
+}
