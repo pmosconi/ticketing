5ef7a3dd08ac10f9bd5813c1381f074f87044957
Paolo Mosconi <pmosconi@gmail.com>
2021-05-02 18:06:07 +0200

    header & signout

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,7 +1,29 @@
+import buildClient from '../api/build-client';
+import Header from '../components/header';
 import 'bootstrap/dist/css/bootstrap.css';
 
-const App = ({ Component, pageProps }) => {
-    return <Component {...pageProps} />
+const AppComponent = ({ Component, pageProps, currentUser }) => {
+    return (
+        <div>
+            <Header currentUser={currentUser} />
+            <Component {...pageProps} />
+        </div>
+    );
 };
 
-export default App;
+AppComponent.getInitialProps = async context => {
+    const client = buildClient(context.ctx);
+    const { data } = await client.get('/api/users/currentuser');
+
+    let pageProps = {};
+    if (context.Component.getInitialProps) {
+        pageProps = await context.Component.getInitialProps(context.ctx);
+    }
+
+    return {
+        pageProps,
+        ...data
+    };
+};
+
+export default AppComponent;
---
+++ client/pages/auth/signout.js
@@ -0,0 +1,18 @@
+import { useEffect } from 'react';
+import Router from 'next/router';
+import useRequest from '../../hooks/useRequest';
+
+const Signout = () => {
+    const { doRequest } = useRequest({
+        url: '/api/users/signout',
+        method: 'post',
+        body: {},
+        onSuccess: () => Router.push('/')
+    });
+
+    useEffect(() => doRequest(), []);
+
+    return <div>Signing out...</div>;
+};
+
+export default Signout;
