861a87d865c9bca236d636aea78efb7e4f5d840b
Paolo Mosconi <pmosconi@gmail.com>
2021-05-09 16:51:55 +0200

    common module setup

--- start.sh
+++ start.sh
@@ -3,3 +3,6 @@ kubectl create secret generic jwt-secret --from-literal=JWT_KEY=$JWT_KEY
 skaffold dev
 
 # thisisunsafe
+
+#debug
+# https://medium.com/collaborne-engineering/remote-debug-nodejs-in-kubernetes-with-vs-code-d0282eae4388
